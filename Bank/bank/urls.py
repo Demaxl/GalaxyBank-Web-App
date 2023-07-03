d52811f3f16135e467be010d2e3d056d71fd9d15
demaxl <aweabdullahi@gmail.com>
2023-07-03 18:30:46 +0100

    Complete Homepage frontend

--- Bank/bank/urls.py
+++ Bank/bank/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="home")
+    path("", views.HomePageView.as_view(), name="home")
 ]
--- Bank/bank/views.py
+++ Bank/bank/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse 
+from django.views.generic import View
 
 
-def index(request):
-    return HttpResponse("Hello! This is galaxy bank app")
+class HomePageView(View):
+    def get(self, request):
+        return render(request, "home.html")
